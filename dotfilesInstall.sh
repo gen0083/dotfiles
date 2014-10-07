40bdbd22ea07d5488df3b2935900e313052d67cf
Gen <archiherewego@gmail.com>
2014-10-07 22:57:33 +0900

    make dotfilesInstall.sh (run this file then dot files are linked home directory)

---
+++ dotfilesInstall.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+ln -s ~/dotfiles/_vimrc ~/.vimrc
+ln -s ~/dotfiles/_gvimrc ~/.gvimrc
+ln -s ~/dotfiles/_bashrc ~/.bashrc
+ln -s ~/dotfiles/_bash_profile ~/.bash_profile
+ln -s ~/dotfiles/vimfiles ~/.vim
+ln -s ~/dotfiles/_gitconfig ~/.gitconfig
+ln -s ~/dotfiles/_gitignore_global ~/.gitignore_global
