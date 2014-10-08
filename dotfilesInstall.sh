8e57185afc0a27b41ede9aa13008c0f7c544bfea
Gen <archiherewego@gmail.com>
2014-10-08 18:16:52 +0900

    fix bugs (既に.vimが存在している場合にシンボリックリンクの処理がうまくで きなかった問題に対応)

--- dotfilesInstall.sh
+++ dotfilesInstall.sh
@@ -5,21 +5,31 @@
 # .files
 # if you create a new dotfile, add the dotfile name to DOTFILES_LIST
 # e.g. create .vimrc then add "vimrc" to DOTFILES_LIST
+DOTFILES_DIRECTORY="dotfiles"
 DOTFILES_LIST="vimrc gvimrc bashrc bash_profile gitconfig\
 	gitignore_global"
 for DOTFILE in $DOTFILES_LIST
 do
 	if [ -f ~/.$DOTFILE ]; then
-		if [ ! -f ~/.$DOTFILE.old ]; then
+		if [ ! -L ~/.$DOTFILE ]; then
+			# カスタマイズ済みのドットファイルを退避
 			mv ~/.$DOTFILE ~/.$DOTFILE.old
+		else
+			# シンボリックリンクを削除
+			rm ~/.$DOTFILE
 		fi
 	fi
-	ln -fs ~/dotfiles/_$DOTFILE ~/.$DOTFILE
+	ln -s ~/$DOTFILES_DIRECTORY/_$DOTFILE ~/.$DOTFILE
 done
 # .directory
 if [ -d ~/.vim ]; then
-	if [ ! -d ~/.vim.old ]; then
+	if [ ! -L ~/.vim ]; then
+		# カスタマイズ済みの.vimを退避する
 		mv ~/.vim ~/.vim.old
+	else
+		# ディレクトリのシンボリックリンクを削除する
+		rm ~/.vim
 	fi
+
 fi
-ln -fs ~/dotfiles/vimfiles ~/.vim
+ln -s ~/$DOTFILES_DIRECTORY/vimfiles/ ~/.vim
