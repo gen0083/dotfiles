ea20a8269ecbc54259cf6834c87ae8ee0f9cc233
Gen <archiherewego@gmail.com>
2014-10-08 17:22:50 +0900

    prepare against  dotfiles are exist

--- dotfilesInstall.sh
+++ dotfilesInstall.sh
@@ -1,8 +1,25 @@
 #! /bin/bash
-ln -s ~/dotfiles/_vimrc ~/.vimrc
-ln -s ~/dotfiles/_gvimrc ~/.gvimrc
-ln -s ~/dotfiles/_bashrc ~/.bashrc
-ln -s ~/dotfiles/_bash_profile ~/.bash_profile
-ln -s ~/dotfiles/vimfiles ~/.vim
-ln -s ~/dotfiles/_gitconfig ~/.gitconfig
-ln -s ~/dotfiles/_gitignore_global ~/.gitignore_global
+
+# this script make symboliclink to user's home directory
+
+# .files
+# if you create a new dotfile, add the dotfile name to DOTFILES_LIST
+# e.g. create .vimrc then add "vimrc" to DOTFILES_LIST
+DOTFILES_LIST="vimrc gvimrc bashrc bash_profile gitconfig\
+	gitignore_global"
+for DOTFILE in $DOTFILES_LIST
+do
+	if [ -f ~/.$DOTFILE ]; then
+		if [ ! -f ~/.$DOTFILE.old ]; then
+			mv ~/.$DOTFILE ~/.$DOTFILE.old
+		fi
+	fi
+	ln -fs ~/dotfiles/_$DOTFILE ~/.$DOTFILE
+done
+# .directory
+if [ -d ~/.vim ]; then
+	if [ ! -d ~/.vim.old ]; then
+		mv ~/.vim ~/.vim.old
+	fi
+fi
+ln -fs ~/dotfiles/vimfiles ~/.vim
