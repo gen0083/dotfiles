47a253257cf2129df88ffc80dd789ee7bc455ec7
Gen <archiherewego@gmail.com>
2023-03-19 21:21:48 +0900

    Update RPROMPT
    vcs_infoが遅いので

--- zsh.d/21_config.zsh
+++ zsh.d/21_config.zsh
@@ -1,14 +1,38 @@
-autoload -Uz vcs_info
 setopt prompt_subst
 
-zstyle ':vcs_info:git:*' check-for-changes true
-zstyle ':vcs_info:git:*' stagedstr "%F{yellow}!"
-zstyle ':vcs_info:git:*' unstagedstr "%F{red}+"
-zstyle ':vcs_info:*' formats "%F{green}%c%u[%b]%f"
-zstyle ':vcs_info:*' actionformats '[%b|%a]'
-precmd(){ vcs_info }
+precmd () {
+  # 直前のコマンド結果
+  if [ $? -eq 0 ];then
+    return_previous_result="%F{green}[%?]%f"
+  else
+    return_previous_result="%F{red}[%?]%f"
+  fi
+  # gitチェック
+  if [ -n "$(git status --short 2>/dev/null)" ];then
+    GIT_HAS_DIFF="✗"
+    GIT_NON_DIFF=""
+  else
+    GIT_HAS_DIFF=""
+    GIT_NON_DIFF="✔"
+  fi
+  # git管理されているか確認
+  git status --porcelain >/dev/null 2>&1
+  if [ $? -ne 0 ];then
+    NOT_GIT_REPOSITORY="1"
+  else
+    NOT_GIT_REPOSITORY="0"
+    BRANCH_NAME=$(git branch --show-current 2>/dev/null)
+  fi
+  # RPROMPTに出力
+  if [ "$NOT_GIT_REPOSITORY" = "1" ];then
+    RPROMPT="${return_previous_result}"
+  elif [ "$GIT_HAS_DIFF" != "" ];then
+    RPROMPT="%f%F{red}${BRANCH_NAME}${GIT_HAS_DIFF}%f ${return_previous_result}"
+  else
+    RPROMPT="%f%F{green}${BRANCH_NAME}${GIT_NON_DIFF}%f ${return_previous_result}"
+  fi
+}
 
 PROMPT='[%f%F{green}%~%f] [@%f%F{cyan}%n%f] [%D / %*]
  %# '
-RPROMPT='${vcs_info_msg_0_}'"$p_color return:[%?]%{${reset_color}%}"
 
