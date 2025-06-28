26435b438c882563246c6ee9b90e02f80a30d2b9
Gen <archiherewego@gmail.com>
2025-06-28 11:59:38 +0900

    add DOCKER_DEFAULT_PLATFORM on Mac

--- zsh.d/00_env.zsh
+++ zsh.d/00_env.zsh
@@ -29,6 +29,8 @@ export NDK_ROOT=/Applications/Android/sdk/ndk-bundle
 # java_home
 . ~/.asdf/plugins/java/set-java-home.zsh
 
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
+
 # 1password
 # export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 
